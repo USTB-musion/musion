5267d6f6c805f6b2ae703ce02bcf793f5002ba6c
李锦 <lijin@lijindeMacBook-Air.local>
2018-09-16 21:41:00 +0800

    将button改为单文件组件

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Button from './button.vue'
+
+Vue.component('m-button', Button)
+
+new Vue({
+    el: '#app'
+})
---
+++ src/button.vue
@@ -0,0 +1,61 @@
+<template>
+    <button class="m-button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#icon-${icon}`"></use>
+        </svg> 
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
+</template>
+
+<script>
+export default {
+    props: ['icon', 'iconPosition']
+}
+</script>
+
+<style lang="less">
+        .m-button {
+            display: inline-flex;
+            justify-content: center;
+            align-items: center;
+            height: var(--button-height);
+            font-size: var(--font-size);
+            padding: 0 1em;
+            border-radius: var(--border-radius);
+            border: 1px solid var(--border-color);
+            background: var(--button-bg);
+            vertical-align: middle;
+
+            &:hover {
+                border-color: var(--border-color-hover);
+            }
+
+            &:active {
+                background-color: var(--button-active-bg);
+            }
+
+            &:focus {
+                outline: none;
+            }
+
+            > .icon {
+                order: 1;
+            }
+
+            > .content {
+                order: 2;
+            }
+
+            &.icon-right {
+                > .icon {
+                    order: 2;
+                }
+                > .content {
+                    order: 1;
+                }
+            }
+
+        }
+</style>
