eb1a9ff0ce0f34b23262a059d2b857d6fa6a1bb5
lijin <18810996718@163.com>
2018-09-17 23:05:40 +0800

    m-button-group第一个组件不用-1px

--- src/button-group.vue
+++ src/button-group.vue
@@ -22,7 +22,9 @@ export default {
         vertical-align: middle;
         > .m-button {
             border-radius: 0;
-            margin-left: -1px;
+            &:not(:first-child) {
+                margin-left: -1px;
+            }
             &:first-child {
                 border-top-left-radius: var(--border-radius);
                 border-bottom-left-radius: var(--border-radius);
