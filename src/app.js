16fdd249db5beddbcfcd1a6f509ecbee5ca7dacc
lijin <18810996718@163.com>
2018-09-18 21:44:36 +0800

    添加单元测试

--- src/app.js
+++ src/app.js
@@ -15,3 +15,85 @@ new Vue({
         loading3: false
     }
 })
+
+
+// 单元测试
+import chai from 'chai'
+const expect = chai.expect
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-setting')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting',
+            loading: true
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-loading')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting',
+            iconPosition: 'right'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const mButton = new Constructor({
+        propsData: {
+            icon: 'setting',
+            iconPosition: 'right'
+        }
+    })
+    mButton.$mount()
+    mButton.$on('click', function() {
+        expect(1).to.eq(1)
+    })
+    let button = mButton.$el
+    button.click()
+}
