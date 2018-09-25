5aa389c2e643c184eeb13ddf39921607b1d3d6f7
lijin <18810996718@163.com>
2018-09-25 23:32:53 +0800

    添加row && col组件

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button.vue'
 import Icon from './icon.vue'
 import ButtonGroup from './button-group.vue'
 import Input from './input.vue'
+import Row from './row.vue'
+import Col from './col.vue'
 
 Vue.component('m-button', Button)
 Vue.component('m-icon', Icon)
 Vue.component('m-button-group', ButtonGroup)
 Vue.component('m-input', Input)
+Vue.component('m-row', Row)
+Vue.component('m-col', Col)
 
 new Vue({
     el: '#app',
