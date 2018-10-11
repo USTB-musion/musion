f1a2799c2c5763bc19a0b34c0324a253b455e44e
lijin <18810996718@163.com>
2018-10-11 08:23:41 +0800

    实现最简单的popover

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="popover" @click="xxx">
-        <slot name="content" v-if="visible"></slot>
+    <div class="popover" @click.stop="xxx">
+        <div class="content-wrapper" @click.stop>
+            <slot name="content" v-if="visible"></slot>
+        </div>
         <slot></slot>
     </div>
 </template>
@@ -16,6 +18,16 @@ export default {
     methods: {
         xxx() {
             this.visible = !this.visible
+            if (this.visible == true) {
+                this.$nextTick(() => {
+                    let eventHandler = () => {
+                        this.visible = false
+                        document.removeEventListener('click', eventHandler)
+                    }
+
+                    document.addEventListener('click', eventHandler)
+                })
+            }
         }
     }
 }
@@ -25,5 +37,13 @@ export default {
     .popover {
         display: inline-block;
         vertical-align: top;
+        position: relative;
+        .content-wrapper {
+            position: absolute;
+            bottom: 100%;
+            left: 0;
+            border: 1px solid red;
+            box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        }
     }
 </style>
